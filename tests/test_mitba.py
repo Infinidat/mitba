f962c5a4f22bce09bcc0cf6fef30b50b4f0b3158
Ayala Shachar <ayalas@infinidat.com>
2016-04-20 11:42:49 +0300

    Use test functions instead of TestCase

--- tests/test_mitba.py
+++ tests/test_mitba.py
@@ -1,195 +1,154 @@
 import functools
-from collections import defaultdict
 import flux
-from unittest import TestCase
 
-from mitba import (CacheData, TimerCacheData, cached_function, cached_method,
-                   cached_method_with_custom_cache, cached_property,
+from mitba import (CacheData, TimerCacheData, cached_function, cached_method_with_custom_cache,
                    clear_cache, clear_cached_entry, populate_cache)
 
-
-class Subject(object):
-
-    def __init__(self):
-        super(Subject, self).__init__()
-        self._counter = 0
-        self._counters = defaultdict(lambda: defaultdict(int))
-
-    @cached_property
-    def prop(self):
-        self._counter += 1
-        return self._counter
-
-    def orig_method(self, value):
-        """some docstring"""
-        self._counters['orig_method'][value] += 1
-        return self._counters['orig_method'][value]
-
-    @cached_method
-    def cached_method_3(self, value):
-        self._counter += 1
-        return self._counter
-
-    @cached_method
-    def cached_method_4(self):
-        self._counter = 1
-        return self._counter
-
-    cached_method_1 = cached_method(orig_method)
-    cached_method_2 = cached_method(orig_method)
+POLL_TIME = 0.05
 
 
-class TestCase(TestCase):
+def test_subject_cached_property(subject):
+    assert subject.prop == 1
 
-    def setUp(self):
-        super(TestCase, self).setUp()
-        self.subject = Subject()
+def test_subject_clear_cache(subject):
+    assert subject.prop == 1
+    clear_cache(subject)
+    assert subject.prop == 2
+
+def test_subject_populate_cache(subject):
+    # pylint: disable=protected-access
+    assert subject._counter == 0
+    populate_cache(subject)
+    assert subject._counter == 1
+
+def test_cached_method_doc(subject):
+    assert subject.cached_method_1.__doc__ == subject.cached_method_2.__doc__ == subject.orig_method.__doc__
+
+def test_cached_method_name(subject):
+    assert subject.cached_method_2.__name__ == subject.cached_method_2.__name__ == subject.orig_method.__name__
+
+def test_cached_method_cached_method(subject):
+    assert subject.cached_method_1(1) == 1
+    assert subject.cached_method_1(1) == 1
+    assert subject.cached_method_2(1) == 2
+
+def test_cached_method_clear_cache(subject):
+    assert subject.cached_method_1(1) == 1
+    assert subject.cached_method_1(1) == 1
+    clear_cache(subject)
+    assert subject.cached_method_1(1) == 2
+
+def test_cached_method_args(subject):
+    clear_cache(subject)
+    assert subject.cached_method_3(1) == 1
+    assert subject.cached_method_3(1) == 1
+    assert subject.cached_method_3(2) == 2
+    assert subject.cached_method_3(2) == 2
+
+def test_cached_method_mutable_args(subject):
+    clear_cache(subject)
+    assert subject.cached_method_3([1]) == 1
+    assert subject.cached_method_3([1]) == 2
+
+def test_cached_method_kwargs(subject):
+    clear_cache(subject)
+    assert subject.cached_method_3(value=1) == 1
+    assert subject.cached_method_3(value=1) == 1
+    assert subject.cached_method_3(value=2) == 2
+    assert subject.cached_method_3(value=2) == 2
+
+def test_cached_method_mutable_kwargs(subject):
+    clear_cache(subject)
+    assert subject.cached_method_3(value=[1]) == 1
+    assert subject.cached_method_3(value=[1]) == 2
+
+def test_cached_method_no_args_and_kwargs(subject):
+    clear_cache(subject)
+    assert subject.cached_method_4() == 1
+    assert subject.prop == 2
+    assert subject.cached_method_4() == 1
+    clear_cache(subject)
 
 
-class CachedPropertyTest(TestCase):
+def test_cached_method_caching_method_on_class_instance():
+    class Foo(object):
 
-    def test__cached_property(self):
-        self.assertEquals(self.subject.prop, 1)
+        def __init__(self):
+            self.counter = 0
 
-    def test__clear_cache(self):
-        self.assertEquals(self.subject.prop, 1)
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.prop, 2)
+        def __inicached_methodt__(self):
+            self.counter = 0
 
-    def test__populate_cache(self):
-        self.assertEquals(self.subject._counter, 0)
-        populate_cache(self.subject)
-        self.assertEquals(self.subject._counter, 1)
+        def invalidate_cache(self):
+            self.__mitba_cache__["tested_method"].invalidate()  # pylint: disable=no-member
 
-poll_time = 0.05
+        @cached_method_with_custom_cache(CacheData)
+        def tested_method(self):
+            self.counter += 1
+            return self.counter
+
+    foo = Foo()  # pylint: disable=blacklisted-name
+    for i in range(1, 5):
+        for _ in range(5):
+            assert foo.tested_method() == i
+        foo.invalidate_cache()
 
 
-class CachedMethodTest(TestCase):
+def test_caching_method_on_external_call():
+    class Bar(object):
 
-    def test__doc(self):
-        self.assertTrue(self.subject.cached_method_1.__doc__ ==
-                        self.subject.cached_method_2.__doc__ == self.subject.orig_method.__doc__)
+        def __init__(self):
+            self.counter = 0
 
-    def test__name(self):
-        self.assertTrue(self.subject.cached_method_2.__name__ ==
-                        self.subject.cached_method_2.__name__ == self.subject.orig_method.__name__)
+        @cached_method_with_custom_cache(functools.partial(TimerCacheData, POLL_TIME))
+        def tested_method(self):
+            self.counter += 1
+            return self.counter
 
-    def test__cached_method(self):
-        self.assertEquals(self.subject.cached_method_1(1), 1)
-        self.assertEquals(self.subject.cached_method_1(1), 1)
-        self.assertEquals(self.subject.cached_method_2(1), 2)
+    bar = Bar()  # pylint: disable=blacklisted-name
+    for _ in range(3):
+        assert bar.tested_method() == 1
 
-    def test__clear_cache(self):
-        self.assertEquals(self.subject.cached_method_1(1), 1)
-        self.assertEquals(self.subject.cached_method_1(1), 1)
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.cached_method_1(1), 2)
+    flux.current_timeline.sleep(POLL_TIME + 0.01)
+    for _ in range(3):
+        assert bar.tested_method() == 2
 
-    def test__args(self):
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.cached_method_3(1), 1)
-        self.assertEquals(self.subject.cached_method_3(1), 1)
-        self.assertEquals(self.subject.cached_method_3(2), 2)
-        self.assertEquals(self.subject.cached_method_3(2), 2)
 
-    def test__mutable_args(self):
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.cached_method_3([1]), 1)
-        self.assertEquals(self.subject.cached_method_3([1]), 2)
+def test_cached_method_single_class_with_two_caches():
+    class FooBar(object):
 
-    def test__kwargs(self):
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.cached_method_3(value=1), 1)
-        self.assertEquals(self.subject.cached_method_3(value=1), 1)
-        self.assertEquals(self.subject.cached_method_3(value=2), 2)
-        self.assertEquals(self.subject.cached_method_3(value=2), 2)
+        def __init__(self):
+            self.timer = 0
+            self.data_counter = 0
 
-    def test__mutable_kwargs(self):
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.cached_method_3(value=[1]), 1)
-        self.assertEquals(self.subject.cached_method_3(value=[1]), 2)
+        @cached_method_with_custom_cache(functools.partial(TimerCacheData, POLL_TIME))
+        def timer_cache(self):
+            self.timer += 1
+            return self.timer
 
-    def test__no_args_and_kwargs(self):
-        clear_cache(self.subject)
-        self.assertEquals(self.subject.cached_method_4(), 1)
-        self.assertEquals(self.subject.prop, 2)
-        self.assertEquals(self.subject.cached_method_4(), 1)
-        clear_cache(self.subject)
+        @cached_method_with_custom_cache(CacheData)
+        def data_cache(self):
+            self.data_counter += 1
+            return self.data_counter
 
-    def test_caching_method_on_class_instance(self):
-        class Foo(object):
+        def invalidate_cache(self):
+            self.__mitba_cache__["data_cache"].invalidate()  # pylint: disable=no-member
 
-            def __init__(self):
-                self.counter = 0
+    foobar = FooBar()
+    for i in range(1, 10):
+        for _ in range(10):
+            assert foobar.data_cache() == i
+            flux.current_timeline.sleep(0.01)
+        foobar.invalidate_cache()
 
-            def invalidate_cache(self):
-                self.__mitba_cache__["tested_method"].invalidate()
+    for _ in range(3):
+        assert foobar.timer_cache() == 1
+        foobar.invalidate_cache()
 
-            @cached_method_with_custom_cache(CacheData)
-            def tested_method(self):
-                self.counter += 1
-                return self.counter
-
-        foo = Foo()
-        for i in range(1, 5):
-            for j in range(5):
-                self.assertEquals(foo.tested_method(), i)
-            foo.invalidate_cache()
-
-    def test_caching_method_on_external_call(self):
-        class Bar(object):
-
-            def __init__(self):
-                self.counter = 0
-
-            @cached_method_with_custom_cache(functools.partial(TimerCacheData, poll_time))
-            def tested_method(self):
-                self.counter += 1
-                return self.counter
-
-        bar = Bar()
-        for _ in range(3):
-            self.assertEquals(bar.tested_method(), 1)
-
-        time.sleep(poll_time + 0.01)
-        for _ in range(3):
-            self.assertEquals(bar.tested_method(), 2)
-
-    def test_single_class_with_two_caches(self):
-        class FooBar(object):
-
-            def __init__(self):
-                self.timer = 0
-                self.data_counter = 0
-
-            @cached_method_with_custom_cache(functools.partial(TimerCacheData, poll_time))
-            def timer_cache(self):
-                self.timer += 1
-                return self.timer
-
-            @cached_method_with_custom_cache(CacheData)
-            def data_cache(self):
-                self.data_counter += 1
-                return self.data_counter
-
-            def invalidate_cache(self):
-                self.__mitba_cache__["data_cache"].invalidate()
-
-        foobar = FooBar()
-        for i in range(1, 10):
-            for j in range(10):
-                self.assertEquals(foobar.data_cache(), i)
-                flux.current_timeline.sleep(0.01)
-            foobar.invalidate_cache()
-
-        for _ in range(3):
-            self.assertEquals(foobar.timer_cache(), 1)
-            foobar.invalidate_cache()
-
-        time.sleep(poll_time + 0.1)
-        for _ in range(3):
-            self.assertEquals(foobar.timer_cache(), 2)
-            foobar.invalidate_cache()
+    flux.current_timeline.sleep(POLL_TIME + 0.1)
+    for _ in range(3):
+        assert foobar.timer_cache() == 2
+        foobar.invalidate_cache()
 
 
 @cached_function
@@ -197,81 +156,58 @@ def func():
     return flux.current_timeline.time()
 
 
-class CachedFunctionTest(TestCase):
 
-    def test_cache_works(self):
-        before = func()
-        after = func()
-        self.assertEqual(before, after)
+def test_cache_function_works():
+    before = func()
+    after = func()
+    assert before == after
 
-    def test_clear_cache_works(self):
-        before = func()
-        clear_cache(func)
-        after = func()
-        self.assertNotEqual(before, after)
+def test_cache_function_clear_cache_works():
+    before = func()
+    clear_cache(func)
+    after = func()
+    assert before != after
 
-    def test_cache_works_after_cleanup(self):
-        first = func()
-        clear_cache(func)
-        second = func()
-        third = func()
-        self.assertNotEqual(first, second)
-        self.assertEqual(second, third)
+def test_cache_function_works_after_cleanup():
+    first = func()
+    clear_cache(func)
+    second = func()
+    third = func()
+    assert first != second
+    assert second == third
 
+def test_clear_cached_entry_on_instance_method__no_args(counter):
+    method = counter.count
+    assert method() == 1
+    assert method() == 1
+    clear_cached_entry(method)
+    assert method() == 2
 
-class Counter(object):
+def test_clear_cached_entry_on_instance_method__with_args(counter):
+    method = counter.sum
+    assert method(1) == 1
+    counter.count()
+    assert method(1) == 1
+    clear_cached_entry(method, 1)
+    assert method(1) == 2
 
-    def __init__(self):
-        super(Counter, self).__init__()
-        self._count = 0
+def test_clear_cached_entry_on_function__with_args():
+    count = 0
 
-    @cached_method
-    def count(self):
-        self._count += 1
-        return self._count
+    @cached_function
+    def sum(num):  # pylint: disable=redefined-builtin
+        return num + count
+    assert sum(1) == 1
+    assert sum(1) == 1
+    count = 2
+    assert sum(1) == 1
+    clear_cached_entry(sum, 1)
+    assert sum(1) == 3
 
-    @cached_method
-    def sum(self, num):
-        return num + self._count
-
-
-class ClearCachedEntryTest(TestCase):
-
-    def test_on_instance_method__no_args(self):
-        counter = Counter()
-        method = counter.count
-        self.assertEquals(method(), 1)
-        self.assertEquals(method(), 1)
-        clear_cached_entry(method)
-        self.assertEquals(method(), 2)
-
-    def test_on_instance_method__with_args(self):
-        counter = Counter()
-        method = counter.sum
-        self.assertEquals(method(1), 1)
-        counter.count()
-        self.assertEquals(method(1), 1)
-        clear_cached_entry(method, 1)
-        self.assertEquals(method(1), 2)
-
-    def test_on_function__with_args(self):
-        count = 0
-
-        @cached_function
-        def sum(num):
-            return num + count
-        self.assertEquals(sum(1), 1)
-        self.assertEquals(sum(1), 1)
-        count = 2
-        self.assertEquals(sum(1), 1)
-        clear_cached_entry(sum, 1)
-        self.assertEquals(sum(1), 3)
-
-    def test_clear_cached_entry_with_mismatching_args_does_nothing(self):
-        counter = Counter()
-        method = counter.sum
-        self.assertEquals(method(1), 1)
-        counter.count()
-        self.assertEquals(method(1), 1)
-        clear_cached_entry(method, 3)
-        self.assertEquals(method(1), 1)
+def test_clear_cached_entry_with_mismatching_args_does_nothing(counter):
+    method = counter.sum
+    assert method(1) == 1
+    counter.count()
+    assert method(1) == 1
+    clear_cached_entry(method, 3)
+    assert method(1) == 1
