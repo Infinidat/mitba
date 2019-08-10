bce6de6e88179e1ffe05e0749e7924737e6c1324
Ayala Shachar <ayalas@infinidat.com>
2019-08-10 23:01:38 +0300

    Switch to PBR

--- Makefile
+++ Makefile
@@ -1,15 +1,13 @@
 default: test
 
 test: env
-	.env/bin/py.test -x tests --cov=mitba --cov-report=html
+	.env/bin/pytest -x tests --cov=mitba --cov-report=html
 
 env: .env/.up-to-date
 
 
-.env/.up-to-date: setup.py Makefile test_requirements.txt
+.env/.up-to-date: setup.py Makefile setup.cfg
 	virtualenv --no-site-packages .env
-	.env/bin/pip install -e .
-	.env/bin/pip install -r ./*.egg-info/requires.txt || true
-	.env/bin/pip install -r test_requirements.txt
+	.env/bin/pip install -e .[testing]
 	touch $@
 
--- setup.py
+++ setup.py
@@ -1,31 +1,9 @@
-import os
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), "mitba", "__version__.py")) as version_file:
-    exec(version_file.read()) # pylint: disable=W0122
 
-_INSTALL_REQUIRES = [
-    "logbook",
-    "flux",
-]
-
-setup(name="mitba",
-      classifiers=[
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          ],
-      description="Python library for caching results from functions and methods",
-      license="BSD3",
-      author="Infinidat Ltd.",
-      author_email="info@infinidat.com",
-      version=__version__, # pylint: disable=E0602
-      packages=find_packages(exclude=["tests"]),
-
-      url="https://github.com/Infinidat/mitba",
-
-      install_requires=_INSTALL_REQUIRES,
-      scripts=[],
-      namespace_packages=[]
-     )
+setup(
+    setup_requires=['pbr>=3.0', 'setuptools>=17.1'],
+    pbr=True,
+    long_description_content_type='text/markdown; charset=UTF-8',
+)
